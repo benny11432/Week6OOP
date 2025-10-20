bea33f3d043f53b7e83281a89b132e5328b436de
BenRyan-STUDENT <g00462517@atu.ie>
2025-10-20 10:31:43 +0100

    part 2

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,22 +1,29 @@
 package ie.atu;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age = 0;
-        while (true) {
-            System.out.print("Enter your age: ");
-            String text = sc.nextLine().trim();
-            try {
-                age = Integer.parseInt(text);
-                System.out.println("Thanks! You entered age = " + age);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input" + e.getMessage());
-            }
+
+        System.out.print("Enter the file name (e.g., students.txt): ");
+        String fileName = sc.nextLine().trim();
+
+        System.out.print("Enter a name to save: ");
+        String name = sc.nextLine().trim();
+
+        try(PrintWriter out = new PrintWriter(new FileWriter(fileName, true)))
+        {
+            out.println("Saved to " + fileName);
+            System.out.println("saved to " + fileName);
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Could not write to file: " + ex.getMessage());
         }
 
     }
