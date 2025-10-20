6cc95b8d5c14bb791ad262dd54bda170b2338a90
BenRyan-STUDENT <g00462517@atu.ie>
2025-10-20 09:26:38 +0100

    setup

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,18 @@
+package ie.atu;
+
+public class TryMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = 0;
+        while(true) {
+            System.out.print("Enter your age: ");
+            String text = sc.nextLine().trim();
+            try {
+                age = Integer.parseInt(text);
+                System.out.println("Thanks! You entered age = " + age);
+                break;
+            } catch (NumberFormatException e) {
+                system.out.println("Invalid input" + e.getMessage());
+            }
+        }
+    }
